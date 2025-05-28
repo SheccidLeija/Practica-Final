f9913858ad71846458a7be756696d90d1fb0c5e5
SheccidLeija <a00837322@tec.mx>
2025-05-27 19:23:53 -0600

    Update ProtectedRoute.js

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Navigate } from 'react-router-dom';
 import AuthService from '../services/auth.service';
 
@@ -11,6 +12,10 @@ const ProtectedRoute = ({ children }) => {
   }
 
   return children;
+};
+
+ProtectedRoute.propTypes = {
+  children: PropTypes.node.isRequired
 };
 
 export default ProtectedRoute; 
